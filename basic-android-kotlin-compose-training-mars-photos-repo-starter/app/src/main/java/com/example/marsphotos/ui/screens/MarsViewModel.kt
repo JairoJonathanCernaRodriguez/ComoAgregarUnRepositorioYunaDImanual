8f220a5582612fb4b1a563b07760c18c2e612077
JairoJonathanCernaRodriguez <113155501+JairoJonathanCernaRodriguez@users.noreply.github.com>
2025-01-30 19:48:14 -0600

    tercer commit

--- basic-android-kotlin-compose-training-mars-photos-repo-starter/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ basic-android-kotlin-compose-training-mars-photos-repo-starter/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -19,47 +19,40 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.marsphotos.MarsPhotosApplication
+import com.example.marsphotos.data.MarsPhotosRepository
 import com.example.marsphotos.model.MarsPhoto
-import com.example.marsphotos.network.MarsApi
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 import java.io.IOException
-import com.example.marsphotos.data.NetworkMarsPhotosRepository
 
-
-/**
- * UI state for the Home screen
- */
 sealed interface MarsUiState {
     data class Success(val photos: String) : MarsUiState
     object Error : MarsUiState
     object Loading : MarsUiState
 }
 
-class MarsViewModel : ViewModel() {
-    /** The mutable State that stores the status of the most recent request */
+class MarsViewModel(private val marsPhotosRepository: MarsPhotosRepository) : ViewModel() {
     var marsUiState: MarsUiState by mutableStateOf(MarsUiState.Loading)
         private set
 
-    /**
-     * Call getMarsPhotos() on init so we can display status immediately.
-     */
+
     init {
         getMarsPhotos()
     }
 
-    /**
-     * Gets Mars photos information from the Mars API Retrofit service and updates the
-     * [MarsPhoto] [List] [MutableList].
-     */
+
     fun getMarsPhotos() {
         viewModelScope.launch {
             marsUiState = MarsUiState.Loading
             marsUiState = try {
-
-                val marsPhotosRepository = NetworkMarsPhotosRepository()
-                val listResult = marsPhotosRepository.getMarsPhotos()                MarsUiState.Success(
+                val listResult = marsPhotosRepository.getMarsPhotos()
+                MarsUiState.Success(
                     "Success: ${listResult.size} Mars photos retrieved"
                 )
             } catch (e: IOException) {
@@ -69,4 +62,15 @@ class MarsViewModel : ViewModel() {
             }
         }
     }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[APPLICATION_KEY] as MarsPhotosApplication)
+                val marsPhotosRepository = application.container.marsPhotosRepository
+                MarsViewModel(marsPhotosRepository = marsPhotosRepository)
+            }
+        }
+    }
 }
+
